f600d9abb6b2f199a8783408ab9c7e51c5f0f236
LogansRun93 <erikmcguire@gmail.com>
2015-10-11 11:47:42 -0500

    Installed SFML and setup project.

---
+++ SFML Game Development Book/SFMLTest.cpp
@@ -0,0 +1,26 @@
+// File used to test that SFML was added to the project correctly. Should open a small window with green circle.
+
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
+	return 0;
+}
